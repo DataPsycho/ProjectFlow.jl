0e8a115acb2d1f4ed4dd306ba97ae7086d7cfc4a
DataPsycho <mr.data.psycho@gmail.com>
2020-07-10 14:22:21 +0200

    Implement profile checker.

--- src/readconfig.jl
+++ src/readconfig.jl
@@ -1,24 +1,12 @@
 const ROOT_DIR = joinpath(homedir(), ".projectflow/profiles")
 
 """
-Check if profile file exist or not.
-# Argument
-- `path` File path of the profile directory.
-"""
-function config_exist(path::String) :: Bool
-    if isfile(path)
-        return true
-    end
-    return false
-end
-
-"""
 Internal profile checker which check if the config file exist or not.
 
 # Argument
 - `path` : File path of the profiles
 """
-function config_exist(path::String) :: Bool
+function profiles_exist(path::String) :: Bool
     if isfile(path)
         return true
     end
@@ -39,6 +27,34 @@ split_string("repo=my_beautiful_repo")
 function split_string(s::String)::Pair
     property = split(s, "=")
     return string(property[1]) => string(property[2])
+end
+
+"""
+Invalidate the loaded properties from profiles config file.
+
+# Argument
+- `p` profiles Dict will all profiles from config file
+"""
+function validate_profiles(p::Dict)
+    all_keys = [
+        "project_root", "projects_dir", "data_dir",
+        "insights_dir", "insights_viz_dir",
+        "insights_data_dir",
+    ]
+    missing_keys = []
+    for (key, value) in p
+        print(key, value)
+        for property in all_keys
+            if !haskey(value, property)
+                push!(missing_keys, property)
+            end
+        end
+    end
+    if !isempty(missing_keys)
+        temp = join(missing_keys, ", ")
+        err = ErrorException("$temp keys does not exist in profile.")
+        throw(err)
+    end
 end
 
 """
@@ -77,5 +93,12 @@ function load_profiles(path::String)::Dict
         err = ErrorException("$path file does not exist")
         throw(err)
     end
+    try
+        validate_profiles(all_profiles)
+    catch ex
+        rethrow(ex)
+    end
     return all_profiles
 end
+
+x = load_profiles(ROOT_DIR)
